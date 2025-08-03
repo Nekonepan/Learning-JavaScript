f6c05d9cd203b1d5c2a6f2c79b9c93549f54c08f
Lutfan Alaudin Naja <lutfanalaudin@gmail.com>
2025-08-03 22:35:23 +0700

    Refactor code structure and remove redundant changes

--- Project/main.js
+++ Project/main.js
@@ -1,4 +1,8 @@
+const prompt = require('prompt-sync')();
+const inquirer = require('inquirer');
 const fs = require('fs');
+const { type } = require('os');
+
 const isi_file = fs.readFileSync('data-karyawan.txt', 'utf-8');
 const baris = isi_file.trim().split('\n');
 
@@ -15,5 +19,52 @@ for (let i = 0; i < baris.length; i++) {
   });
 }
 
-console.log("Jumlah data : ", data.length);
-console.table(data);
+async function main_menu() {
+  const { menu } = await inquirer.prompt([
+    {
+      type: 'list',
+      name: 'menu',
+      message: 'Pilih Menu : ',
+      choices: [
+        'Tampilkan Semua Data',
+        'Tambah Data Baru',
+        'Cari Karyawan',
+        'Keluar'
+      ]
+    }
+  ]);
+
+  switch (menu) {
+    case 'Tampilkan Semua Data':
+      tampilkan_data();
+      break;
+
+    case 'Tambah Data Baru':
+      tambah_data();
+      break;
+
+    case 'Cari Karyawan':
+      cari_data();
+      break;
+
+    case 'Keluar':
+      console.log('Keluar dari program.');
+      process.exit();
+  }
+
+  await main_menu();
+}
+
+main_menu();
+
+function tampilkan_data() {
+  console.log("Data Karyawan : ");
+}
+
+async function tambah_data() {
+  console.log("Untuk Tambah Data");
+}
+
+async function cari_data() {
+  console.log("Untuk Cari Data");
+}
