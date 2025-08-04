884f2994f68927fa1d8aa257a298bc3986918dc9
Lutfan Alaudin Naja <lutfanalaudin@gmail.com>
2025-08-04 14:05:38 +0700

    Update data-karyawan.txt with new employee records and enhance main.js to include sorting functionality by employee ID.

--- Project/main.js
+++ Project/main.js
@@ -94,7 +94,7 @@ async function search_by_id() {
   if (hasil) {
     console.log("========== DATA DITEMUKAN =========");
     console.log(`ID : "${cari_id}" ditemukan.`);
-    console.table([hasil]);
+    console.table(hasil);
   } else {
     console.log(`Data dengan ID "${cari_id}" tidak ditemukan.`);
   }
@@ -161,6 +161,69 @@ async function cari_data() {
 }
 // ================================================================================================
 
+// SORTING DATA BERDASARKAN ID KARYAWAN ===========================================================
+async function sort_by_id() {
+  console.log("========= URUTKAN DATA BERDASARKAN ID ========");
+
+  const { arah } = await inquirer.prompt([
+    {
+      type: "list",
+      name: "arah",
+      message: "Pilih arah pengurutan data : ",
+      choices: ["Ascending (A-Z)", "Descending (Z-A)"],
+    },
+  ]);
+
+  const data_sort = [...data];
+
+  data_sort.sort((a, b) => {
+    return arah.includes("Ascending")
+      ? a.ID.localeCompare(b.ID)
+      : b.ID.localeCompare(a.ID);
+  });
+
+  console.log("\n========= HASIL SORTING ==========");
+  console.table(data_sort);
+
+  const { action } = await inquirer.prompt([
+    {
+      type: "list",
+      name: "action",
+      message: "Simpan hasil sorting?",
+      choices: ["Simpan hasil sorting ke file", "Jangan simpan"],
+    },
+  ]);
+
+  // VALIDASI AKSI -----------------------------------------------------------------------------------------------------
+  if (action === "Simpan hasil sorting ke file") {
+    const { confirm_action } = await inquirer.prompt([
+      {
+        type: "confirm",
+        name: "confirm_action",
+        message:
+          "Apakah anda yakin ingin menyimpan data hasil sorting ke file? (Aksi ini akan menimpa semua data sebelumnya)",
+      },
+    ]);
+  // -------------------------------------------------------------------------------------------------------------------
+
+    if (confirm_action) {
+      const new_data =
+        data_sort
+          .map((item) => `${item.ID}|${item.NAMA}|${item.JABATAN}|${item.TELP}`)
+          .join("\n") + "\n";
+
+      fs.writeFileSync("data-karyawan.txt", new_data);
+      data = data_sort;
+      console.log("Data telah disimpan ke file.");
+    } else {
+      console.log("Aksi dibatalkan. Data tidak disimpan.");
+    }
+  } else {
+    console.log("Data tidak disimpan.");
+  }
+}
+// ================================================================================================
+
 // MENU PILIHAN ===================================================================================
 async function main_menu() {
   const { menu } = await inquirer.prompt([
@@ -169,37 +232,45 @@ async function main_menu() {
       name: "menu",
       message: "Pilih Menu : ",
       choices: [
-        "Tampilkan Semua Data",
-        "Tambah Data Baru",
-        "Cari Karyawan",
-        "Keluar",
+        "1. Tampilkan Semua Data",
+        "2. Tambah Data Baru",
+        "3. Urutkan Data",
+        "4. Cari Karyawan",
+        "5. Keluar",
       ],
     },
   ]);
 
   switch (menu) {
-    case "Tampilkan Semua Data": {
+    case "1. Tampilkan Semua Data": {
       console.log("\n");
       tampilkan_data();
       console.log("\n");
       break;
     }
 
-    case "Tambah Data Baru": {
+    case "2. Tambah Data Baru": {
       console.log("\n");
       await tambah_data();
       console.log("\n");
       break;
     }
 
-    case "Cari Karyawan": {
+    case "3. Urutkan Data": {
+      console.log("\n");
+      await sort_by_id();
+      console.log("\n");
+      break;
+    }
+
+    case "4. Cari Karyawan": {
       console.log("\n");
       await cari_data();
       console.log("\n");
       break;
     }
 
-    case "Keluar": {
+    case "5. Keluar": {
       console.log("\n");
       console.log("Keluar dari program.");
       process.exit();
