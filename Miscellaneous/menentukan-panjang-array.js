97c8fb055dcfb0020cf8a21c14d1b3d3f2ff843f
Lutfan Alaudin Naja <lutfanalaudin@gmail.com>
2025-10-07 12:32:26 +0700

    Add menentukan-panjang-array.js to define an array and calculate its length

---
+++ Miscellaneous/menentukan-panjang-array.js
@@ -0,0 +1,14 @@
+// Mendefinisikan sebuah array
+let buah = [
+  53, 87, 42, 65, 91, 59, 70, 48, 77, 95, 82, 43, 99, 64, 84, 58, 47, 90, 72,
+  50, 94, 81, 49, 68, 73, 46, 60, 98, 55, 71, 80, 66, 41, 78, 85, 97, 54, 67,
+  75, 63, 52, 83, 61, 44, 76, 89, 62, 56, 79, 45, 86, 92, 74, 57, 88, 93, 96,
+  70, 59, 64, 41, 66, 80, 84, 47, 90, 42, 71, 77, 43, 75, 94, 69, 85, 49, 53,
+  81, 63, 88, 95, 44, 91, 58, 67, 60, 46, 55, 62, 50, 79,
+];
+
+// Menghitung panjang array menggunakan properti .length
+let panjangArrayBuah = buah.length;
+
+// Menampilkan hasil
+console.log("Panjang array 'buah' adalah: " + panjangArrayBuah); // Output: Panjang array 'buah' adalah: 4
