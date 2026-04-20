310f4d6a2ddd1fbd0dc9dc23276318977d4b6c97
Nekonepan <lutfanalaudin@gmail.com>
2026-04-20 22:45:09 +0700

    Implement bubble sort algorithm in a new bubble.js file

---
+++ Algorithms-Strategy/Bubble-Sort/bubble.js
@@ -0,0 +1,17 @@
+function bubbleSort(arr) {
+  let n = arr.length;
+  let temp;
+
+  for (let i = 0; i < n - 1; i++) {
+    for (let j = 0; j < n - i - 1; j++) {
+      if (arr[j] > arr[j + 1]) {
+        temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+      }
+    }
+  }
+  return arr;
+}
+
+module.exports = bubbleSort;
