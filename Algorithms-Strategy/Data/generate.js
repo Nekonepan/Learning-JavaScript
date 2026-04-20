e50f4188c0714ff8580c446c1f538fed273f5381
Nekonepan <lutfanalaudin@gmail.com>
2026-04-20 22:58:01 +0700

    Refactor data generation in generate.js to create multiple JSON files with varying sizes from 1000 to 10000, increasing by 1000. Update random number range to 0-99999.

--- Algorithms-Strategy/Data/generate.js
+++ Algorithms-Strategy/Data/generate.js
@@ -3,12 +3,14 @@ const fs = require("fs");
 function generateData(n) {
   let arr = [];
   for (let i = 0; i < n; i++) {
-    arr.push(Math.floor(Math.random() * 1000)); // 0 - 999
+    arr.push(Math.floor(Math.random() * 100000));
   }
   return arr;
 }
 
-let data = generateData(5000);
-
-// simpan ke JSON
-fs.writeFileSync("data.json", JSON.stringify(data, null, 2));
+// generate dari 1000 sampai 10000
+for (let i = 1000; i <= 10000; i += 1000) {
+  let data = generateData(i);
+  fs.writeFileSync(`Data/data_${i}.json`, JSON.stringify(data));
+  console.log(`Data ${i} berhasil dibuat`);
+}
